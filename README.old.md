33ac4149f8e792453d58721638b5ec1f1102bead
kiritocode1 <kathawalearyan9@gmail.com>
2022-09-02 11:04:22 +0530

    CHORE :libraries install

--- README.old.md
+++
@@ -1,2 +0,0 @@
-# How_i_made_this_website
-BLOG SPA : How I made This Website
--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { NextUIProvider } from '@nextui-org/react';
+import { Button ,Text,Progress} from '@nextui-org/react';
+
+const BtnComponent = () => <Button color="secondary">Click me</Button>;
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App bg-black h-[50rem]  pt-20">
+      <NextUIProvider>
+
+        <Text h1 size={60}
+        css={{
+          textGradient: "45deg, $blue600 10%, $pink600 50%",
+        }}
+          weight="bold" shadow ># Comming soon 👨‍🚀</Text>
+        <div className='flex items-center justify-center'>
+        <div className='w-40'>
+            <Progress value={15} shadow color="primary" status="primary" /></div></div>
+        
+      </NextUIProvider>
     </div>
   );
 }
